d85e3563d30ca2b5b83c2305290be7f5b82e9ee1
Adrian Albu <2412rock@gmail.com>
2024-04-27 21:07:19 +0300

    init

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Controller]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        [Route("api/login")]
+        public IActionResult Login()
+        {
+            return Ok("login success");
+        }
+    }
+}
