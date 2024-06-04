509246ccbb9c52e6a60de036885b89a798dd4315
Adrian Albu <2412rock@gmail.com>
2024-06-04 14:10:55 +0300

    Health check endpoint

---
+++ Controllers/HealthCheck.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OverflowBackend.Controllers
+{
+    [ApiController]
+    public class HealthCheck : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/health")]
+        public IActionResult Health()
+        {
+            return Ok("Online");
+        }
+    }
+}
