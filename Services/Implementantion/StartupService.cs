71b97ec302072dfcc73e594df02f9bf3f088178d
Adrian Albu <2412rock@gmail.com>
2024-09-14 19:39:38 +0300

    Log server started

--- Services/Implementantion/StartupService.cs
+++ Services/Implementantion/StartupService.cs
@@ -6,14 +6,17 @@ namespace OverflowBackend.Services.Implementantion
     public class StartupService: IStartupService
     {
         private readonly OverflowDbContext _context;
+        private readonly ILogger _logger;
 
-        public StartupService(OverflowDbContext context)
+        public StartupService(OverflowDbContext context, ILogger logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         public void Initialize()
         {
+            _logger.LogInformation("Server started");
             // Read a value from the database
             var gameVersion = _context.Versions.FirstOrDefault(e => e.VersionID == 1);
             if (gameVersion != null)
