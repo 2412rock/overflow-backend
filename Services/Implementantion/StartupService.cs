0e216965a7b1e65f4605fff62affaac1c47035f7
Adrian Albu <2412rock@gmail.com>
2024-09-14 19:47:03 +0300

    Log when server starts

--- Services/Implementantion/StartupService.cs
+++ Services/Implementantion/StartupService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNet.SignalR.Messaging;
+using Microsoft.EntityFrameworkCore;
 using OverflowBackend.Services.Interface;
 
 namespace OverflowBackend.Services.Implementantion
@@ -6,17 +7,27 @@ namespace OverflowBackend.Services.Implementantion
     public class StartupService: IStartupService
     {
         private readonly OverflowDbContext _context;
-        private readonly ILogger _logger;
 
-        public StartupService(OverflowDbContext context, ILogger logger)
+        public StartupService(OverflowDbContext context)
         {
             _context = context;
-            _logger = logger;
         }
 
         public void Initialize()
         {
-            _logger.LogInformation("Server started");
+            try
+            {
+                var logFilePath = $"/app/logs/{DateTime.Now:yyyy-MM-dd}.txt";
+                // EnsureDirectoryExists($"C:/Users/{Environment.UserName}/OverflowLogs");
+
+                if (!File.Exists(logFilePath))
+                {
+                    File.Create(logFilePath).Dispose(); // Dispose to release the file handle
+                }
+                File.AppendAllText(logFilePath, "Server started" + Environment.NewLine);
+            }
+            catch(DirectoryNotFoundException e) { }
+            
             // Read a value from the database
             var gameVersion = _context.Versions.FirstOrDefault(e => e.VersionID == 1);
             if (gameVersion != null)
