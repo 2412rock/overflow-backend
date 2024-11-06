f13e380db46358d9ace584a8d213d4e32b4092c0
Adrian Albu <2412rock@gmail.com>
2024-11-06 16:31:46 +0200

    update ip

--- Program.cs
+++ Program.cs
@@ -67,7 +67,7 @@ builder.Services.AddSignalR();
 var saPassword = Environment.GetEnvironmentVariable("SA_PASSWORD");
 var localIp = Environment.GetEnvironmentVariable("LOCAL_IP");
 var env = builder.Environment.EnvironmentName;
-string hostIp = env == "Development" ? "192.168.1.117" : localIp;
+string hostIp = env == "Development" ? "192.168.1.117" : "10.244.225.232";
 
 Console.WriteLine($"Connecting to DB IP {hostIp}");
 builder.Services.AddDbContext<OverflowDbContext>(options =>
