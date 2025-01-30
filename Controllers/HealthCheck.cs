804dd9b3eb89f73f421262628348e30100cf0ea0
Adrian Albu <2412rock@gmail.com>
2025-01-30 15:24:23 +0200

    Update healthcheck to include db connection

--- Controllers/HealthCheck.cs
+++ Controllers/HealthCheck.cs
@@ -1,16 +1,43 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OverflowBackend.Models.Response.DorelAppBackend.Models.Responses;
+using OverflowBackend.Services;
 
 namespace OverflowBackend.Controllers
 {
     [ApiController]
     public class HealthCheck : ControllerBase
     {
+        private readonly OverflowDbContext _dbContext;
+        public HealthCheck(OverflowDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         [HttpGet]
         [Route("api/health")]
-        public IActionResult Health()
+        public async Task<IActionResult> Health()
         {
-            return Ok("Online");
+            var maybe = new Maybe<string>();
+            try
+            {
+                // test data base connection is working
+                var version = await _dbContext.Versions.FirstOrDefaultAsync(e => e.VersionID == 1);
+                if(version != null)
+                {
+                    maybe.SetSuccess("Healthy");
+                }
+                else
+                {
+                    maybe.SetException("Version is null");
+                }
+            }
+            catch(Exception e)
+            {
+                maybe.SetException("Database error occured " + e.Message);
+            }
+            
+            return Ok(maybe);
         }
     }
 }
