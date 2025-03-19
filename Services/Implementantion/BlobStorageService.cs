b122141ccd7e77c0ee196ce0817fe0f5bb5be213
Adrian Albu <2412rock@gmail.com>
2025-03-19 14:52:20 +0200

    aaaa

--- Services/Implementantion/BlobStorageService.cs
+++ Services/Implementantion/BlobStorageService.cs
@@ -32,7 +32,7 @@ namespace OverflowBackend.Services.Implementantion
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Exception occured at minio {endpoint} {accessKey} {accessKey}");
+                Console.WriteLine($"Exception occured at minio {endpoint} {accessKey} {secretKey}");
                 throw;
             }
         }
